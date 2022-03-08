96bb447f529d7dcb847092e70865dd3cb8223188
MatheusCaires04 <matheus-bini@hotmail.com>
2022-03-08 16:59:01 -0300

    Header pronto

---
+++ starbucks-coffe/slider.js
@@ -0,0 +1,40 @@
+let time = 5000,
+    currentImageIndex = 0,
+    currentImageMobileIndex = 0,
+    images = document
+                .querySelectorAll("#slider img"),
+    imagesMobile = document
+                .querySelectorAll("#slider-mobile img")
+    max = images.length,
+    maxMobile = imagesMobile.length;
+
+function nextImage() {
+
+    images[currentImageIndex]
+        .classList.remove("selected")
+    imagesMobile[currentImageMobileIndex]
+        .classList.remove("selected-mobile")
+
+    currentImageIndex++
+    currentImageMobileIndex++
+
+    if(currentImageIndex >= max)
+        currentImageIndex = 0
+
+    if(currentImageMobileIndex >= maxMobile)
+    currentImageMobileIndex = 0
+
+    images[currentImageIndex]
+        .classList.add("selected")
+    images[currentImageMobileIndex]
+        .classList.add("selected-mobile")
+}
+
+function start() {
+    setInterval(() => {
+        // troca de image
+        nextImage()
+    }, time)
+}
+
+window.addEventListener("load", start)
